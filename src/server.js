794326f92f0307eb5a7273c33b8529994c164675
Morten Jansrud <morten@founder.no>
2019-07-30 13:36:12 +0200

    Fjern proxy

--- src/server.js
+++ src/server.js
@@ -7,16 +7,16 @@ require("dotenv").config({
   path: "/var/run/secrets/nais.io/vault/environment.env"
 });
 
-app.use(
-  "/person/tilbakemeldinger-api/proxy",
-  proxy("https://api-gw-q0.oera.no/tilbakemeldingsmottak")
-);
-
 app.get("/person/tilbakemeldinger-api/internal/isAlive", (req, res) =>
   res.sendStatus(200)
 );
 app.get("/person/tilbakemeldinger-api/internal/isReady", (req, res) =>
   res.sendStatus(200)
+);
+
+app.use(
+  "/person/tilbakemeldinger-api/",
+  proxy("https://api-gw-q0.oera.no/tilbakemeldingsmottak")
 );
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
