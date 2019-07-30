48e816b84fcefd4e57cc7669fb43614de8f7dbc4
Morten Jansrud <morten@founder.no>
2019-07-30 15:29:31 +0200

    Sett headers

--- src/server.js
+++ src/server.js
@@ -22,6 +22,16 @@ app.get("/person/tilbakemeldinger-api/internal/isReady", (req, res) =>
   res.sendStatus(200)
 );
 
-app.use("/person/tilbakemeldinger-api/", proxy(TILBAKEMELDINGSMOTTAK_URL));
+app.use(
+  "/person/tilbakemeldinger-api/",
+  proxy(TILBAKEMELDINGSMOTTAK_URL, {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers[
+        TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_USERNAME
+      ] = TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_PASSWORD;
+      return proxyReqOpts;
+    }
+  })
+);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
