2542e85632b1fcd8a05176ce50aa77dc8744f79a
Morten Jansrud <morten@founder.no>
2019-07-30 13:18:48 +0200

    Legg til proxy og prettier

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const proxy = require("express-http-proxy");
+const express = require("express");
+const app = express();
+const port = 8080;
 
-app.get('/person/tilbakemeldinger-api/', (req, res) => res.send('Hello World!'));
-app.get("/person/tilbakemeldinger-api/internal/isAlive", (req, res) => res.sendStatus(200));
-app.get("/person/tilbakemeldinger-api/internal/isReady", (req, res) => res.sendStatus(200));
+app.use(
+  "/person/tilbakemeldinger-api/proxy",
+  proxy("https://api-gw-q0.oera.no/tilbakemeldingsmottak")
+);
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get("/person/tilbakemeldinger-api/internal/isAlive", (req, res) =>
+  res.sendStatus(200)
+);
+app.get("/person/tilbakemeldinger-api/internal/isReady", (req, res) =>
+  res.sendStatus(200)
+);
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
