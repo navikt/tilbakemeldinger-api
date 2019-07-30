74dd2b7081ad263b9f492acfd72a83edd10b2137
Morten Jansrud <morten@founder.no>
2019-07-30 10:43:11 +0200

    Legg til internal

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/person/tilbakemeldinger-api/', (req, res) => res.send('Hello World!'));
-app.get("/person/tilbakemeldinger-api/isAlive", (req, res) => res.sendStatus(200));
-app.get("/person/tilbakemeldinger-api/isReady", (req, res) => res.sendStatus(200));
+app.get("/person/tilbakemeldinger-api/internal/isAlive", (req, res) => res.sendStatus(200));
+app.get("/person/tilbakemeldinger-api/internal/isReady", (req, res) => res.sendStatus(200));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
