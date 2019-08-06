f26555c6c38f4b6be20da58d40364788d6c13b40
Morten Jansrud <morten@founder.no>
2019-08-06 13:12:52 +0200

    Legg til proxy for oppdaterenhetsinfo

--- src/server.js
+++ src/server.js
@@ -3,49 +3,33 @@ const cookies = require("cookie-parser");
 const express = require("express");
 const decodeJWT = require("jwt-decode");
 const app = express();
-const port = 8080;
+const port = 3002;
 const BASE_URL = "/person/tilbakemeldinger-api";
-
-require("dotenv").config({
-  path: "/var/run/secrets/nais.io/vault/environment.env"
-});
-
-const {
-  TILBAKEMELDINGSMOTTAK_URL,
-  TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_USERNAME,
-  TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_PASSWORD
-} = process.env;
+const VAULT_PATH = "/var/run/secrets/nais.io/vault/environment.env";
+const dotenv = require("dotenv").config({ path: VAULT_PATH });
+const { setEnheterProxyHeaders, setMottakProxyHeaders } = require("./headers");
 
 app.use(cookies());
 app.get(`${BASE_URL}/internal/isAlive`, (req, res) => res.sendStatus(200));
 app.get(`${BASE_URL}/internal/isReady`, (req, res) => res.sendStatus(200));
-
 app.get(`${BASE_URL}/fodselsnr`, (req, res) =>
   res.send({ fodselsnr: decodeJWT(req.cookies["selvbetjening-idtoken"]).sub })
 );
 
-const onProxyReq = (proxyReq, req, res) => {
-  const authToken = req.cookies["selvbetjening-idtoken"];
-
-  if (authToken) {
-    proxyReq.setHeader("Authorization", `Bearer ${authToken}`);
-  }
-
-  proxyReq.setHeader(
-    TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_USERNAME,
-    TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_PASSWORD
-  );
-
-  Object.keys(req.headers).forEach(key => {
-    proxyReq.setHeader(key, req.headers[key]);
-  });
-};
+app.use(
+  proxy(`${BASE_URL}/enheter`, {
+    target: process.env.ENHETERRS_URL,
+    pathRewrite: { "^/person/tilbakemeldinger-api": "" },
+    onProxyReq: setEnheterProxyHeaders,
+    changeOrigin: true
+  })
+);
 
 app.use(
   proxy(BASE_URL, {
-    target: TILBAKEMELDINGSMOTTAK_URL,
+    target: process.env.TILBAKEMELDINGSMOTTAK_URL,
     pathRewrite: { "^/person/tilbakemeldinger-api": "" },
-    onProxyReq: onProxyReq,
+    onProxyReq: setMottakProxyHeaders,
     changeOrigin: true
   })
 );
