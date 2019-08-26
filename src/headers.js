2c61aac507d3828baf32e5c8793e4e28cc46c776
Morten Jansrud <morten.jansrud@nav.no>
2019-08-26 13:41:21 +0200

    Fetch sts token

--- src/headers.js
+++ src/headers.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+const base64 = require("base-64");
 /*
   Set headers for proxy requests
  */
@@ -9,21 +11,53 @@ const setEnheterProxyHeaders = (proxyReq, req, res) => {
   );
 };
 
-const setMottakProxyHeaders = (proxyReq, req, res) => {
-  const authToken = req.cookies["selvbetjening-idtoken"];
+const setMottakProxyHeaders = async (proxyReq, req, res) => {
+  const {
+    TILBAKEMELDINGER_API_SECURITY_TOKEN_SERVICE_TOKEN_APIKEY_USERNAME,
+    TILBAKEMELDINGER_API_SECURITY_TOKEN_SERVICE_TOKEN_APIKEY_PASSWORD
+  } = process.env;
 
-  if (authToken) {
-    proxyReq.setHeader("Authorization", `Bearer ${authToken}`);
-  }
-
-  proxyReq.setHeader(
-    process.env.TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_USERNAME,
-    process.env.TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_PASSWORD
+  // Fetch service user token
+  const STS_BASIC_AUTH = base64.encode(
+    `${process.env.SRVTILBAKEMELDINGER_API_USERNAME}:${process.env.SRVTILBAKEMELDINGER_API_PASSWORD}`
   );
 
-  Object.keys(req.headers).forEach(key => {
-    proxyReq.setHeader(key, req.headers[key]);
-  });
+  const STS_HEADERS = {
+    Authentication: `Basic ${STS_BASIC_AUTH}`,
+    [TILBAKEMELDINGER_API_SECURITY_TOKEN_SERVICE_TOKEN_APIKEY_USERNAME]: TILBAKEMELDINGER_API_SECURITY_TOKEN_SERVICE_TOKEN_APIKEY_PASSWORD
+  };
+
+  const STS_OPTIONS = {
+    headers: STS_HEADERS
+  };
+
+  await fetch(process.env.SECURITY_TOKEN_SERVICE_TOKEN_URL, STS_OPTIONS)
+    .then(stsRes => stsRes.json())
+    .then(stsRes => {
+      // Success, add service user token
+      console.log(stsRes);
+      proxyReq.setHeader("Authorization", `Bearer ${stsRes.token}`);
+
+      // Add user token from cookie
+      const authToken = req.cookies["selvbetjening-idtoken"];
+
+      if (authToken) {
+        proxyReq.setHeader("Nav-Selvbetjening-Token", `Bearer ${authToken}`);
+      }
+
+      proxyReq.setHeader(
+        process.env.TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_USERNAME,
+        process.env.TILBAKEMELDINGER_API_TILBAKEMELDINGSMOTTAK_APIKEY_PASSWORD
+      );
+
+      Object.keys(req.headers).forEach(key => {
+        proxyReq.setHeader(key, req.headers[key]);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      throw new Error(error);
+    });
 };
 
 module.exports = {
