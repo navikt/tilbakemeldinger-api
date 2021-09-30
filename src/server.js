afaaffdbee640b8cc3337d6b4e76e9e4b9fed2f6
anders-nom <anders@nomerstad.com>
2021-09-30 16:26:28 +0200

    cors credentials

--- src/server.js
+++ src/server.js
@@ -23,11 +23,12 @@ app.get(`${BASE_URL}/fodselsnr`, (req, res) =>
   res.send({ fodselsnr: decodeJWT(req.cookies["selvbetjening-idtoken"]).sub })
 );
 
-if (process.env.ENV === 'dev') {
-    app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://person.dev.nav.no');
-        next();
-    })
+if (process.env.ENV === "dev") {
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://person.dev.nav.no");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 }
 
 // Proxied requests
