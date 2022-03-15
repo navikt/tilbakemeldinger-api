6ddbe1e1d33fbc9d5e82bc284e6c6390b01d81b1
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-03-15 14:48:19 +0100

    Revert "Logging"
    This reverts commit 2d254e8dd363495fcfba7f66a1dcf6e3e8adeef0.

--- src/server.js
+++ src/server.js
@@ -29,9 +29,8 @@ app.use(cookies());
 app.get(`/internal/isAlive`, (req, res) => res.sendStatus(200));
 app.get(`/internal/isReady`, (req, res) => res.sendStatus(200));
 
-app.get(`/fodselsnr`, (req, res) => {
-  console.log(req.cookies["selvbetjening-idtoken"])
-  res.send({ fodselsnr: decodeJWT(req.cookies["selvbetjening-idtoken"]).pid })}
+app.get(`/fodselsnr`, (req, res) =>
+  res.send({ fodselsnr: decodeJWT(req.cookies["selvbetjening-idtoken"]).sub })
 );
 
 // Proxied requests
